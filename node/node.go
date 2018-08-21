c3f23e4e2eaf9b18bf87386b352b342c8b86b6d2
walterBishop <walterkangluo@gmail.com>
2018-08-21 21:46:44 +0800

    feature# Adding config.json to define default setting
    1. Adding config.json;
    2. Adding gossipswitch when new a node;

--- node/node.go
+++ node/node.go
@@ -5,6 +5,7 @@ import (
 	"github.com/DSiSc/galaxy/consensus"
 	"github.com/DSiSc/galaxy/participates"
 	"github.com/DSiSc/galaxy/role"
+	"github.com/DSiSc/gossipswitch/gossipswitch"
 	"github.com/DSiSc/justitia/node/config"
 	"github.com/DSiSc/txpool/common/log"
 	"github.com/DSiSc/txpool/core"
@@ -17,12 +18,26 @@ type Node struct {
 	participates participates.Participates
 	role         role.Role
 	consensus    consensus.Consensus
+	txSwitch     *gossipswitch.GossipSwitch
+	blockSwitch  *gossipswitch.GossipSwitch
 }
 
 // init a node fimply
 func NewNode() (*Node, error) {
 
 	nodeConfig := config.NewNodeConfig()
+
+	// gossip switch
+	txSwitch, err := gossipswitch.NewGossipSwitchByType(gossipswitch.TxSwitch)
+	if err != nil {
+		log.Error("Init txSwitch failed.")
+		return nil, fmt.Errorf("TxSwitch failed.")
+	}
+	blkSwitch, err := gossipswitch.NewGossipSwitchByType(gossipswitch.BlockSwitch)
+	if err != nil {
+		log.Error("Init block switch failed.")
+		return nil, fmt.Errorf("BlkSwitch failed.")
+	}
 
 	// txpool
 	txpoolConf := core.TxPoolConfig{
@@ -52,12 +67,29 @@ func NewNode() (*Node, error) {
 	}
 
 	node := &Node{
-		config: nodeConfig,
-		txpool: txpool,
+		config:       nodeConfig,
+		txpool:       txpool,
 		participates: participates,
-		role: role,
-		consensus: consensus,
+		role:         role,
+		consensus:    consensus,
+		txSwitch:     txSwitch,
+		blockSwitch:  blkSwitch,
 	}
 
 	return node, nil
+}
+
+func (self *Node) Start() error {
+	// start
+	_, err := self.participates.GetParticipates()
+	if nil != err {
+		log.Error("Get participates failed.")
+		return fmt.Errorf("Get participates failed.")
+	}
+	_, err = self.role.RoleAssignments()
+	if nil != err {
+		log.Error("Role assignments failed.")
+		return fmt.Errorf("Role assignments failed.")
+	}
+	return nil
 }
