54702d72563cd2bdb89c81f98cbfcd8e6abfa968
walterBishop <walterkangluo@gmail.com>
2018-09-10 07:12:34 -0700

    optimise# Add test cases to node.

--- node/node_test.go
+++ node/node_test.go
@@ -1,35 +1,94 @@
 package node
 
 import (
+	"fmt"
 	"github.com/DSiSc/craft/types"
-	"github.com/DSiSc/justitia/common"
-	"github.com/DSiSc/justitia/tools/events"
+	"github.com/DSiSc/gossipswitch"
+	"github.com/DSiSc/monkey"
 	"github.com/stretchr/testify/assert"
-	"math/big"
 	"testing"
-)
 
-// mock a transaction
-func mock_transactions(num int) []*types.Transaction {
-	to := make([]types.Address, num)
-	for m := 0; m <= num; m++ {
-		for j := 0; j < types.AddressLength; j++ {
-			to[m][j] = byte(m)
-		}
-	}
-	amount := new(big.Int)
-	txList := make([]*types.Transaction, 0)
-	for i := 1; i <= num; i++ {
-		tx := common.NewTransaction(uint64(i), to[i], amount.SetUint64(uint64(i)), uint64(i), amount, nil, to[0])
-		txList = append(txList, tx)
-	}
-	return txList
-}
+	"github.com/DSiSc/blockchain"
+	blockchainc "github.com/DSiSc/blockchain/config"
+	"github.com/DSiSc/galaxy/consensus"
+	consensusc "github.com/DSiSc/galaxy/consensus/config"
+	"github.com/DSiSc/galaxy/participates"
+	"github.com/DSiSc/galaxy/participates/config"
+	"github.com/DSiSc/galaxy/role"
+	rolec "github.com/DSiSc/galaxy/role/config"
+	"github.com/DSiSc/justitia/tools/events"
+	"github.com/DSiSc/validator/tools/account"
+	"reflect"
+)
 
 func TestNewNode(t *testing.T) {
 	assert := assert.New(t)
 
+	monkey.Patch(gossipswitch.NewGossipSwitchByType, func(switchType gossipswitch.SwitchType) (*gossipswitch.GossipSwitch, error) {
+		return nil, fmt.Errorf("mock gossipswitch error")
+	})
 	service, err := NewNode()
+	assert.NotNil(err)
+	assert.Nil(service)
+	assert.Equal(err, fmt.Errorf("txswitch init failed"))
+	monkey.Unpatch(gossipswitch.NewGossipSwitchByType)
+
+	monkey.Patch(gossipswitch.NewGossipSwitchByType, func(switchType gossipswitch.SwitchType) (*gossipswitch.GossipSwitch, error) {
+		return nil, fmt.Errorf("mock gossipswitch error")
+	})
+	service, err = NewNode()
+	assert.NotNil(err)
+	assert.Nil(service)
+	assert.Equal(err, fmt.Errorf("txswitch init failed"))
+	monkey.Unpatch(gossipswitch.NewGossipSwitchByType)
+	/*
+		var oport *gossipswitch.OutPort
+		monkey.PatchInstanceMethod(reflect.TypeOf(oport), "BindToPort", func (_ *gossipswitch.OutPort, _ gossipswitch.OutPutFunc) error {
+			return fmt.Errorf("bind error")
+		})
+		service, err = NewNode()
+		assert.NotNil(err)
+		assert.Nil(service)
+		assert.Equal(err, fmt.Errorf("registe txpool failed"))
+		monkey.UnpatchInstanceMethod(reflect.TypeOf(oport), "BindToPort")
+	*/
+	monkey.Patch(blockchain.InitBlockChain, func(_ blockchainc.BlockChainConfig) error {
+		return fmt.Errorf("mock blockchain error")
+	})
+	service, err = NewNode()
+	assert.NotNil(err)
+	assert.Nil(service)
+	assert.Equal(err, fmt.Errorf("blockchain init failed"))
+	monkey.Unpatch(blockchain.InitBlockChain)
+
+	monkey.Patch(participates.NewParticipates, func(conf config.ParticipateConfig) (participates.Participates, error) {
+		return nil, fmt.Errorf("mock participates error")
+	})
+	service, err = NewNode()
+	assert.NotNil(err)
+	assert.Nil(service)
+	assert.Equal(err, fmt.Errorf("participates init failed"))
+	monkey.Unpatch(participates.NewParticipates)
+
+	monkey.Patch(role.NewRole, func(_ participates.Participates, _ account.Account, _ rolec.RoleConfig) (role.Role, error) {
+		return nil, fmt.Errorf("mock role error")
+	})
+	service, err = NewNode()
+	assert.NotNil(err)
+	assert.Nil(service)
+	assert.Equal(err, fmt.Errorf("role init failed"))
+	monkey.Unpatch(role.NewRole)
+
+	monkey.Patch(consensus.NewConsensus, func(_ participates.Participates, _ consensusc.ConsensusConfig) (consensus.Consensus, error) {
+		return nil, fmt.Errorf("mock consensus error")
+	})
+	service, err = NewNode()
+	assert.NotNil(err)
+	assert.Nil(service)
+	assert.Equal(err, fmt.Errorf("consensus init failed"))
+	monkey.Unpatch(consensus.NewConsensus)
+
+	service, err = NewNode()
 	assert.Nil(err)
 	assert.NotNil(service)
 
@@ -45,6 +104,7 @@ func TestNewNode(t *testing.T) {
 	assert.Nil(nodeService.validator)
 	event := types.GlobalEventCenter.(*events.Event)
 	assert.Equal(3, len(event.Subscribers))
+
 }
 
 func TestNode_Start(t *testing.T) {
@@ -66,4 +126,35 @@ func TestEventRegister(t *testing.T) {
 	EventRegister()
 	event := types.GlobalEventCenter.(*events.Event)
 	assert.Equal(t, 3, len(event.Subscribers))
+}
+
+func TestEventUnregister(t *testing.T) {
+	assert := assert.New(t)
+	// init event center
+	types.GlobalEventCenter = events.NewEvent()
+	EventRegister()
+	eventC := types.GlobalEventCenter.(*events.Event)
+	assert.Equal(3, len(eventC.Subscribers))
+}
+
+func TestNode_Restart(t *testing.T) {
+	var nodeService *Node
+	var node *Node
+	monkey.PatchInstanceMethod(reflect.TypeOf(node), "Stop", func(_ *Node) error {
+		return fmt.Errorf("node stop error")
+	})
+	err := nodeService.Restart()
+	assert.Equal(t, err, fmt.Errorf("node stop error"))
+	monkey.UnpatchInstanceMethod(reflect.TypeOf(node), "Stop")
+
+	monkey.PatchInstanceMethod(reflect.TypeOf(node), "Stop", func(_ *Node) error {
+		return nil
+	})
+	monkey.PatchInstanceMethod(reflect.TypeOf(node), "Start", func(_ *Node) {
+		return
+	})
+	err = nodeService.Restart()
+	assert.Nil(t, err)
+	monkey.UnpatchInstanceMethod(reflect.TypeOf(node), "Stop")
+	monkey.UnpatchInstanceMethod(reflect.TypeOf(node), "Start")
 }
