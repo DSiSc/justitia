d080cb5e2dededbbeca44c9a4ad4a616d47231de
walterBishop <walterkangluo@gmail.com>
2018-09-10 06:19:06 -0700

    optimise# Add test cases to events.

--- tools/events/event_test.go
+++ tools/events/event_test.go
@@ -1,12 +1,12 @@
 package events
 
 import (
+	"errors"
 	"fmt"
+	"github.com/DSiSc/craft/log"
 	"github.com/DSiSc/craft/types"
 	"github.com/stretchr/testify/assert"
 	"testing"
-
-	"github.com/DSiSc/craft/log"
 )
 
 func TestNewEvent(t *testing.T) {
@@ -14,6 +14,7 @@ func TestNewEvent(t *testing.T) {
 	assert := assert.New(t)
 	var EventSaveBlock types.EventType = 1
 	var EventReplyTx types.EventType = 2
+	var EventNoneTx types.EventType = 3
 
 	var subscriber1 types.EventFunc = func(v interface{}) {
 		log.Info("TEST: subscriber1 event func #1.")
@@ -30,16 +31,25 @@ func TestNewEvent(t *testing.T) {
 	assert.NotEqual(sub1, sub2)
 	event.Subscribe(EventSaveBlock, subscriber2)
 	event.Subscribe(EventReplyTx, subscriber2)
+
 	log.Info("TEST: Notify...")
 	err := event.Notify(EventSaveBlock, nil)
 	assert.Nil(err)
 	err = event.Notify(EventSaveBlock, fmt.Errorf("callback failed"))
 	assert.Nil(err)
+
 	log.Info("TEST: Notify All...")
 	errs := event.NotifyAll()
 	assert.Equal(0, len(errs))
+
+	log.Info("TEST: UnSubscribe who has subscribe...")
 	err = event.UnSubscribe(EventReplyTx, sub1)
 	assert.Nil(err)
+
+	log.Info("TEST: Unsubscribe who has not subscrib...")
+	err = event.UnSubscribe(EventNoneTx, nil)
+	assert.Equal(err, errors.New("event type not exist"))
+
 	log.Info("TEST: Notify All after unsubscribe sub1...")
 	errs = event.NotifyAll()
 	assert.Equal(0, len(errs))
