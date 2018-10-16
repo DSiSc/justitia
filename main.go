67ffe15596796ee18f21bc16d5f76d69b781ea7c
walterBishop <walterkangluo@gmail.com>
2018-10-16 23:58:35 +0800

    feature# Add sysSignalProcess() to process system signal.

--- main.go
+++ main.go
@@ -3,7 +3,25 @@ package main
 import (
 	"github.com/DSiSc/craft/log"
 	"github.com/DSiSc/justitia/node"
+	"github.com/DSiSc/justitia/tools/signal"
+	"os"
+	"syscall"
 )
+
+func sysSignalProcess(node node.NodeService) {
+	sysSignalProcess := signal.NewSignalSet()
+	sysSignalProcess.RegisterSysSignal(syscall.SIGINT, func(os.Signal, interface{}) {
+		log.Warn("handle signal SIGINT.")
+		node.Stop()
+		os.Exit(1)
+	})
+	sysSignalProcess.RegisterSysSignal(syscall.SIGTERM, func(os.Signal, interface{}) {
+		log.Warn("handle signal SIGTERM.")
+		node.Stop()
+		os.Exit(1)
+	})
+	go sysSignalProcess.CatchSysSignal()
+}
 
 func main() {
 	node, err := node.NewNode()
@@ -16,5 +34,6 @@ func main() {
 		}
 	}()
 	node.Start()
+	sysSignalProcess(node)
 	node.Wait()
 }
