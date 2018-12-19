ff82c28980e97bb923066acf5e642e378f911ee9
bchain618 <bchain618@gmail.com>
2018-12-19 21:08:56 +0800

    optimise# add unit test to improve code coverage

--- propagator/block_propagator_test.go
+++ propagator/block_propagator_test.go
@@ -1,14 +1,18 @@
 package propagator
 
 import (
+	"github.com/DSiSc/craft/types"
 	"github.com/DSiSc/justitia/tools/events"
+	"github.com/DSiSc/monkey"
 	"github.com/DSiSc/p2p"
 	pconf "github.com/DSiSc/p2p/config"
+	"github.com/DSiSc/p2p/message"
 	"github.com/stretchr/testify/assert"
+	"reflect"
 	"testing"
 )
 
-func mockP2P() p2p.P2PAPI {
+func mockP2P() *p2p.P2P {
 	config := &pconf.P2PConfig{}
 	p, _ := p2p.NewP2P(config, events.NewEvent())
 	return p
@@ -31,6 +35,54 @@ func TestBlockPropagator_Start(t *testing.T) {
 	err = bp.Start()
 	assert.Nil(err)
 	assert.Equal(int32(1), bp.isRuning)
+	bp.Stop()
+}
+
+func TestBlockPropagator_RecvMsg(t *testing.T) {
+	defer monkey.UnpatchAll()
+	assert := assert.New(t)
+	blockOut := make(chan interface{})
+	msgChan := make(chan *p2p.InternalMsg)
+	p2pN := mockP2P()
+	monkey.PatchInstanceMethod(reflect.TypeOf(p2pN), "MessageChan", func(this *p2p.P2P) <-chan *p2p.InternalMsg {
+		return msgChan
+	})
+
+	bp, err := NewBlockPropagator(p2pN, blockOut, events.NewEvent())
+	assert.Nil(err)
+	assert.NotNil(bp)
+	err = bp.Start()
+	assert.Nil(err)
+	assert.Equal(int32(1), bp.isRuning)
+
+	block := &types.Block{}
+	bmsg := &message.Block{
+		Block: block,
+	}
+	iMsg := &p2p.InternalMsg{
+		Payload: bmsg,
+	}
+	go func() {
+		msgChan <- iMsg
+	}()
+
+	block1 := <-blockOut
+	assert.Equal(block, block1)
+	bp.Stop()
+}
+
+func TestBlockPropagator_BlockEventFunc(t *testing.T) {
+	assert := assert.New(t)
+	blockOut := make(chan interface{})
+	bp, err := NewBlockPropagator(mockP2P(), blockOut, events.NewEvent())
+	assert.Nil(err)
+	assert.NotNil(bp)
+	err = bp.Start()
+	assert.Nil(err)
+	assert.Equal(int32(1), bp.isRuning)
+
+	block := &types.Block{}
+	bp.eventCenter.Notify(types.EventBlockCommitted, block)
 	bp.Stop()
 }
 
--- propagator/tx_propagator_test.go
+++ propagator/tx_propagator_test.go
@@ -1,7 +1,12 @@
 package propagator
 
 import (
+	"github.com/DSiSc/craft/types"
+	"github.com/DSiSc/monkey"
+	"github.com/DSiSc/p2p"
+	"github.com/DSiSc/p2p/message"
 	"github.com/stretchr/testify/assert"
+	"reflect"
 	"testing"
 )
 
@@ -22,6 +27,54 @@ func TestTxPropagator_Start(t *testing.T) {
 	err = tp.Start()
 	assert.Nil(err)
 	assert.Equal(int32(1), tp.isRuning)
+	tp.Stop()
+}
+
+func TestTxPropagator_RecvTx(t *testing.T) {
+	defer monkey.UnpatchAll()
+	assert := assert.New(t)
+	txOut := make(chan interface{})
+	msgChan := make(chan *p2p.InternalMsg)
+	p2pN := mockP2P()
+	monkey.PatchInstanceMethod(reflect.TypeOf(p2pN), "MessageChan", func(this *p2p.P2P) <-chan *p2p.InternalMsg {
+		return msgChan
+	})
+
+	tp, err := NewTxPropagator(p2pN, txOut)
+	assert.Nil(err)
+	assert.NotNil(tp)
+	err = tp.Start()
+	assert.Nil(err)
+	assert.Equal(int32(1), tp.isRuning)
+
+	tx := &types.Transaction{}
+	tmsg := &message.Transaction{
+		Tx: tx,
+	}
+	iMsg := &p2p.InternalMsg{
+		Payload: tmsg,
+	}
+	go func() {
+		msgChan <- iMsg
+	}()
+	tx1 := <-txOut
+	assert.Equal(tx, tx1)
+	tp.Stop()
+}
+
+func TestTxPropagator_TxSwitchOutPutFunc(t *testing.T) {
+	assert := assert.New(t)
+	txOut := make(chan interface{})
+	tp, err := NewTxPropagator(mockP2P(), txOut)
+	assert.Nil(err)
+	assert.NotNil(tp)
+	err = tp.Start()
+	assert.Nil(err)
+	assert.Equal(int32(1), tp.isRuning)
+
+	outFunc := tp.TxSwitchOutPutFunc()
+	outFunc(&types.Transaction{})
+	outFunc(&types.Block{})
 	tp.Stop()
 }
 
